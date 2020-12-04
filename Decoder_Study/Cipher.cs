221d1339acc41ca629949451a26b69cf025fc5ca
Zaharov Andrey <zaharov.andrey161@gmail.com>
2020-12-04 04:28:16 +0300

    Step Range bugfix

---
+++ Decoder_Study/Cipher.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Decoder_Study
+{
+    internal abstract class Cipher
+    {
+        private readonly string name;
+
+        public Cipher(string n = "empty")
+        {
+            name = n;
+        }
+
+        public virtual string Decode(string code, int parametr)
+        {
+            return "Empty:" + code;
+        }
+
+        public virtual string Encode(string code, int parametr)
+        {
+            return "Empty:" + code;
+        }
+    }
+}
