05f2e7f14607a160d822234dd84ce4a6c2cc81a4
Zaharov Andrey <zaharov.andrey161@gmail.com>
2020-12-04 04:28:16 +0300

    Added base Cipher class. Added Caesar:Cipher class.
    Added full-working caesar encode-decode. Added template for future
    ciphers. Added Hidding textbox for cipher parameters. Small refactories.

---
+++ Decoder_Study/Caesar.cs
@@ -0,0 +1,55 @@
+﻿namespace Decoder_Study
+{
+    internal class Caesar : Cipher
+    {
+        public Caesar(string n = "caesar") : base(n)
+        { }
+
+        private static string UniversalCode(string code, int step)
+        {
+            string alphabet_ru_upper = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ";
+            string alphabet_ru_lower = alphabet_ru_upper.ToLower();
+            string alphabet_en_upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            string alphabet_en_lower = alphabet_en_upper.ToLower();
+
+            string output = string.Empty;
+
+            for (int i = 0; i < code.Length; i++)
+            {
+                if (alphabet_ru_upper.Contains(code[i].ToString()))
+                {
+                    int index = (alphabet_ru_upper.Length + alphabet_ru_upper.IndexOf(code[i]) + step % alphabet_ru_upper.Length) % alphabet_ru_upper.Length;
+                    output += alphabet_ru_upper[index];
+                }
+                else if (alphabet_ru_lower.Contains(code[i].ToString()))
+                {
+                    int index = (alphabet_ru_lower.Length + alphabet_ru_lower.IndexOf(code[i]) + step % alphabet_ru_lower.Length) % alphabet_ru_lower.Length;
+                    output += alphabet_ru_lower[index];
+                }
+                else if (alphabet_en_upper.Contains(code[i].ToString()))
+                {
+                    int index = (alphabet_en_upper.Length + alphabet_en_upper.IndexOf(code[i]) + step % alphabet_en_upper.Length) % alphabet_en_upper.Length;
+                    output += alphabet_en_upper[index];
+                }
+                else if (alphabet_en_lower.Contains(code[i].ToString()))
+                {
+                    int index = (alphabet_en_lower.Length + alphabet_en_lower.IndexOf(code[i]) + step % alphabet_en_lower.Length) % alphabet_en_lower.Length;
+                    output += alphabet_en_lower[index];
+                }
+                else
+                { output += code[i]; }
+            }
+            return output;
+        }
+
+        public override string Encode(string code, int step)
+        {
+            return UniversalCode(code, step);
+        }
+
+        public override string Decode(string code, int step)
+        {
+            return UniversalCode(code, -step);
+        }
+    }
+}
--- Decoder_Study/MainWindow.xaml.cs
+++ Decoder_Study/MainWindow.xaml.cs
@@ -20,9 +20,47 @@ namespace Decoder_Study
     /// </summary>
     public partial class MainWindow : Window
     {
+        private Cipher currentCipher;
+        private int parametr;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Encode_Click(object sender, RoutedEventArgs e)
+        {
+            Int32.TryParse(Parametr_TextBox.Text, out parametr);
+            Output_TextBox.Text = currentCipher.Encode(Input_TextBox.Text, parametr);
+            Console.WriteLine("Encode");
+        }
+
+        private void Decode_Click(object sender, RoutedEventArgs e)
+        {
+            Int32.TryParse(Parametr_TextBox.Text, out parametr);
+            Output_TextBox.Text = currentCipher.Decode(Input_TextBox.Text, parametr);
+            Console.WriteLine("Decode");
+        }
+
+        private void Cipher_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            string selectedCipher = (sender as ComboBox).SelectedItem.ToString();
+            if (selectedCipher == "System.Windows.Controls.ComboBoxItem: 1: Шифр Цезаря")
+            {
+                currentCipher = new Caesar();
+                Parametr_Label.Visibility = Visibility.Visible;
+                Parametr_Label.IsEnabled = true;
+                Parametr_TextBox.Visibility = Visibility.Visible;
+                Parametr_TextBox.IsEnabled = true;
+            }
+            else
+            {
+                Parametr_Label.Visibility = Visibility.Hidden;
+                Parametr_Label.IsEnabled = false;
+                Parametr_TextBox.Visibility = Visibility.Hidden;
+                Parametr_TextBox.IsEnabled = false;
+            }
+            Console.WriteLine(selectedCipher);
         }
     }
 }
