82aba1eb999122a00d40f9f46dcc3e4accd6bf61
Захаров Андрей <zaharov.andrey161@gmail.com>
2021-03-14 23:32:03 +0300

    Added base XOR cipher #10

---
+++ Decoder_Study/Ciphers/Xor.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Decoder_Study
+{
+    internal class Xor : Cipher
+    {
+        public Xor() : base("Xor")
+        {
+            parameterRequired = true;
+            parameterHintText = "Феникс";
+        }
+
+        public override string Decode(string code, string parameter)
+        {
+            string result = "";
+            for (int i = 0; i < code.Length; i++)
+            {
+                result += (char)(code[i] ^ parameter[i % parameter.Length]);
+                Console.WriteLine(result);
+            }
+            return result;
+        }
+
+        public override string Encode(string code, string parameter)
+        {
+            return Decode(code, parameter);
+        }
+    }
+}
