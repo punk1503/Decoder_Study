0db0fa963d6cd5af92cccbab12c4a75bd9842479
Захаров Андрей <zaharov.andrey161@gmail.com>
2021-04-06 14:34:15 +0300

    Reworked Vigenere decoding #20

--- Decoder_Study/Ciphers/Vigenere.cs
+++ Decoder_Study/Ciphers/Vigenere.cs
@@ -26,7 +26,6 @@ namespace Decoder_Study
                 */
                 if (alphabet_en_upper.Contains(char.ToUpper(code[i])))
                 {
-                    Console.WriteLine("RESULT IS " + result);
                     int offset_index = alphabet_en_upper.IndexOf(char.ToUpper(parameter[i % parameter.Length]));
                     char new_letter = alphabet_en_upper[(alphabet_en_upper.IndexOf(char.ToUpper(code[i])) + offset_index) % alphabet_en_upper.Length];
                     result += char.IsUpper(code[i]) ? new_letter : char.ToLower(new_letter);
@@ -42,12 +41,14 @@ namespace Decoder_Study
         public override string Decode(string code, string parameter)
         {
             string result = "";
-            string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            string alphabet_en_upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
             for (int i = 0; i < code.Length; i++)
             {
-                if (alphabet.Contains(code[i]))
+                if (alphabet_en_upper.Contains(char.ToUpper(code[i])))
                 {
-                    result += alphabet[(alphabet.IndexOf(code[i]) + alphabet.Length - alphabet.IndexOf(parameter[i % parameter.Length])) % alphabet.Length];
+                    int offset_index = alphabet_en_upper.IndexOf(char.ToUpper(parameter[i % parameter.Length]));
+                    char new_letter = alphabet_en_upper[(alphabet_en_upper.IndexOf(char.ToUpper(code[i])) + alphabet_en_upper.Length - offset_index) % alphabet_en_upper.Length];
+                    result += char.IsUpper(code[i]) ? new_letter : char.ToLower(new_letter);
                 }
                 else
                 {
