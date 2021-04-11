97f8ab39edd95f90b99e18df306907438e27c79b
Захаров Андрей <zaharov.andrey161@gmail.com>
2021-04-11 16:54:28 +0300

    Fixed Hex paragrpah name

--- Decoder_Study/Ciphers/Hex.cs
+++ Decoder_Study/Ciphers/Hex.cs
@@ -9,7 +9,7 @@ namespace Decoder_Study
     {
         private Encoding enc_1251 = Encoding.GetEncoding(1251);
 
-        public Hex() : base("Xor")
+        public Hex() : base("Hex")
         {
             docDir = docDir + name + ".xaml";
             parameterRequired = false;
